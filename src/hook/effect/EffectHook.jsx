6edba415f304b016d94bb007546d49a48fff1090
임성준 <zkfmak10@gmail.com>
2025-04-10 21:17:18 +0900

    useEffect When Set Object

--- src/hook/effect/EffectHook.jsx
+++ src/hook/effect/EffectHook.jsx
@@ -4,6 +4,7 @@ export default function EffectHook() {
     const [id, setId] = useState(Math.random());
     const [domain, setDomain] = useState('naver.com');
     const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState({});
     const [authenticated, setAuthenticated] = useState('');
     const domains = ['naver.com', 'gmail.com', 'kakao.com'];
 
@@ -20,13 +21,26 @@ export default function EffectHook() {
     }
 
     const onLogin = () => {
+        if (!id.trim()) {
+            setErrors({...errors, idError: '아이디를 입력해주세요.'});
+            return;
+        }
+        
+        if (!password.trim()) {
+            setErrors({...errors, passwordError: '비밀번호를 입력해주세요.'});
+            return;
+        }
+
+        setErrors({});
         setAuthenticated(`${id}@${domain},${password} 로그인 성공`);
+        
     }
 
     return(
         <div>
             <div>
             <input type="text" value={id} onChange={onChangeId} />
+            {errors.idError && <span>{errors.idError}</span>}
             <span>@</span>
             {domain === '' ? <input type="text" /> : null}
             <select onChange={onChangeDomain}>
@@ -37,6 +51,7 @@ export default function EffectHook() {
             </select>
             </div>
             <input type="password" value={password} onChange={onChangePassword} />
+            {errors.passwordError && <span>{errors.passwordError}</span>}
             <button onClick={onLogin}>로그인</button>
             <span>{authenticated}</span>
         </div>
