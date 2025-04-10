87306c8bef9447d71c083097cbc74259c81b5541
임성준 <zkfmak10@gmail.com>
2025-04-10 20:24:51 +0900

    edit README.md

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import EffectHook from './effectHook.jsx'
+import EffectHook from './effectHook'
+
 function App() {
-  const [domain, setDomain] = useState('naver.com');
-  const domains = ['naver.com', 'gmail.com', 'kakao.com'];
   return (
     <EffectHook />
   )
